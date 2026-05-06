29bb4efbab4889167a9def6a98e55c23db0c308d
Ivo Julca <ivojulca@hotmail.com>
2026-05-06 12:59:16 -0500

    Fix build

--- lib/translators/DoodadsTranslator.ts
+++ lib/translators/DoodadsTranslator.ts
@@ -135,7 +135,6 @@ export abstract class DoodadsTranslator {
 
             const flags: flag = outBufferToJSON.readByte();
             doodad.flags = {
-                // @ts-expect-error
                 visible: flags === 1 || flags === 2,
                 solid: flags === 2
             };
